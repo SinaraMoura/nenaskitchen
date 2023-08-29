c78761fb353f1866b1f2025e1a29f8320cade988
Sinara Moura <sinara1295@hotmail.com>
2023-08-29 20:48:50 -0300

    implementando página de artigos

--- src/app/(pages)/articles/page.tsx
+++ src/app/(pages)/articles/page.tsx
@@ -1,5 +1,20 @@
-export default function Articles() {
+'use client';
+import ArticlesContainer from "@/app/components/ArticlesContainer"
+import { fetchWrapper } from "@/utils/fetchWrapper";
+
+export default async function Articles() {
+    const response = await fetchWrapper('/articles/list');
+
     return (
-        <h1>Articles</h1>
+        <div className="w-full container px-5">
+            <h1 className="text-2xl font-medium mb-4">Articles</h1>
+            <div className="grid grid-cols-3 gap-4">
+                {response.map((article: any) => {
+                    return (
+                        <ArticlesContainer articles={article} />
+                    )
+                })}
+            </div>
+        </div>
     )
 }
---
+++ src/app/components/ArticlesContainer.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Image from "next/image";
+
+export default function ArticlesContainer({ articles }: any) {
+    return (
+
+        <div className="rounded flex flex-col mb-4 cursor-pointer">
+            <div className="w-full p-3 h-[150px] relative bg-black bg-opacity-25 rounded-3xl shadow bg-cover bg-center"
+                style={{ backgroundImage: `url(/carnes.webp)` }}
+            />
+            <h3 className="text-normal pb-1 font-bold">{articles.title}</h3>
+        </div>
+
+    )
+}
