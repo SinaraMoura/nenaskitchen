ddf34fd9fc85bf2fdcd96dc1fbf0f16190f13383
Sinara Moura <sinara1295@hotmail.com>
2023-10-26 11:27:25 -0300

    adicionando getServerSideProps na página de artigos

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,17 +1,14 @@
-
+import { GetServerSideProps } from "next";
 import ArticlesContainer from "../../components/ArticlesContainer"
 import { fetchWrapper } from "../../utils/fetchWrapper";
 
-export default async function Articles() {
-    const response = await fetchWrapper('/articles/list', {
-        method: 'GET'
-    });
+export default function Articles({ articles }: any) {
 
     return (
         <div className="m-auto h-full container px-5 mb-8">
             <h1 className="text-xl font-bold text-scale-gray-7 mt-8 mb-8">{`Nenas's Ticken > Artigos`}</h1>
             <div className="w-full grid grid-cols-1  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 pb-8">
-                {response.map((article: any) => {
+                {articles.map((article: any) => {
                     return (
                         <ArticlesContainer articles={article} />
                     )
@@ -19,4 +16,21 @@ export default async function Articles() {
             </div>
         </div>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    let articles = [];
+    try {
+        articles = await fetchWrapper('/articles/list', {
+            method: 'GET',
+        });
+    } catch (error) {
+        articles = [];
+        console.log(error);
+    }
+    return {
+        props: {
+            articles
+        }
+    }
 }
