7072cb1fada0da1dfc549d6a35c5014f07a3a576
SinaraMoura <sinara1295@hotmail.comm>
2024-02-03 13:42:09 -0300

    retornando as receitas categorizadas

--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -2,33 +2,38 @@ import { api } from "@/utils/api";
 import RecipesContainer from "../../components/RecipesContainer";
 import { GetServerSideProps } from "next";
 
-export default function Recipes({ recipes }: any) {
+export default function Recipes({ recipes: categories }: any) {
+  categories.sort((a: any, b: any) => a.category.localeCompare(b.category));
+
     return (
         <div className="m-auto h-full container px-5 mb-8">
             <h1 className="text-xl font-bold text-scale-gray-7 mt-8 mb-8">{`Nenas's Ticken > Receitas`}</h1>
-            <div className="w-full gap-4 pb-8 grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 ">
-                {recipes.map((recipe: any) => {
-                    return (
-                        <RecipesContainer key={recipe._id} recipes={recipe} />
-                    )
-                })}
-            </div>
+            {categories.map((category: any) => (
+                <div key={category.category}>
+                <h2 className="text-xl font-bold text-scale-gray-7 mt-8 mb-8">{category.category}</h2>
+                <div className="w-full gap-4 pb-8 grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2">
+                    {category.recipes.map((recipe: any) => (
+                    <RecipesContainer key={recipe._id} recipes={recipe} />
+                    ))}
+                </div>
+                </div>
+            ))}
         </div>
     )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    let recipes = [];
+    let categories = [];
     try {
-        const response = await api.get('/recipes/list'); 7
-        recipes = response.data;
+        const response = await api.get('/recipes/category'); 7
+        categories = response.data;
 
     } catch (error) {
-        recipes = [];
+        categories = [];
     }
     return {
         props: {
-            recipes
+            recipes: categories
         }
     }
 }
